91052a04198d7fed07f17770b526a47cfb23826c
Ahmedish <yamood02@gmail.com>
2023-05-22 21:12:58 -0400

    Fixed bug with start button and added break time functionality

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ let valueSec = parseInt(timerSeconds.innerHTML); //get text value
 let valueMin = parseInt(timerMinutes.innerHTML);
 let mess = document.getElementById("message");
 
+let timerRunning = 0;
 
 /**
  * Bugs that need fixing
- * 1. pressing start button while timer is running
- * 2. when value is less than 10 still display the leading 0
+ * 1. when value is less than 10 still display the leading 0
  * 
  * New features
  * 1. add functionality for break time
@@ -17,17 +17,32 @@ let mess = document.getElementById("message");
  */
 
 button.onclick = function () {
-    mess.innerHTML = "Focus Time"
+    button.disabled = true;
+    timerRunning = 1;
     let intrvl = setInterval(updateTimer, 1000);
     console.log("hi")
 
     let msElapsed = ((valueMin * 60) + valueSec) * 1000;
     console.log(msElapsed);
     setTimeout(function () {
+        button.disabled = false;
+        timerRunning = 0;
         clearInterval(intrvl)
         let audio = new Audio('alarm.mp3');
         audio.play();
-        mess.innerHTML = "Break Time"
+        if (mess.innerHTML == "Focus Time") {
+            mess.innerHTML = "Break Time"
+            timerMinutes.innerHTML = "5";
+            timerSeconds.innerHTML = "00";
+            valueMin = 5;
+            valueSec = 0;
+        } else if (mess.innerHTML == "Break Time") {
+            mess.innerHTML = "Focus Time"
+            timerMinutes.innerHTML = "25";
+            timerSeconds.innerHTML = "00";
+            valueMin = 25;
+            valueSec = 0;
+        }
     }, msElapsed);
 }
 
